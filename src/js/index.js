d00782b8fe8dc6167d4d21401e1b5739fecf350b
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-02-15 14:59:51 +0100

    principio

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
 
-sayHello();
+const screen = document.getElementById('screen');
+const rootStyles = document.documentElement.style;
+
+window.addEventListener('scroll', e => {
+  //scroll = 0 -> 1
+  //scroll = 100 -> 2.4
+
+  const totalScroll = e.target.scrollingElement.scrollHeight;
+  const currentScroll = e.target.scrollingElement.scrollTop;
+
+  const currentScale = (currentScroll * 2.4) / (totalScroll - window.innerHeight) + 1;
+
+  rootStyles.setProperty('--scale', currentScale);
+});
