4beaa068b2021aa7da0cd690f0361ccaad1c7272
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-02-16 14:57:34 +0100

    clase

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,10 @@
 import '../scss/styles.scss';
 
 const screen = document.getElementById('screen');
+const pipeLight = document.getElementById('pipe-light');
+const label = document.getElementById('label');
+const input = document.getElementById('input');
+
 const rootStyles = document.documentElement.style;
 
 window.addEventListener('scroll', e => {
@@ -11,8 +15,22 @@ window.addEventListener('scroll', e => {
   const totalScroll = e.target.scrollingElement.scrollHeight;
   const currentScroll = e.target.scrollingElement.scrollTop;
 
-  const currentScale =
-    (currentScroll * 1.5) / (totalScroll - window.innerHeight) + 1;
+  const currentScale = (currentScroll * 1.5) / (totalScroll - window.innerHeight) + 1;
 
   rootStyles.setProperty('--scale', currentScale);
+});
+const changeTheme = value => {
+  if (value) {
+    //enciende farola
+    pipeLight.classList.remove('pipe--on');
+    screen.classList.remove('background--night');
+  } else {
+    pipeLight.classList.add('pipe--on');
+    screen.classList.add('background--night');
+  }
+};
+
+label.addEventListener('click', e => {
+  console.dir(input.checked);
+  changeTheme(input.checked);
 });
